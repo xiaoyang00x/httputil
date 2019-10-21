4c9d9d7a80e0e7286857e74fd2b3bf811370dc17
lijiaxiong <lijiaxiong@benmu-health.com>
2019-10-21 20:27:33 +0800

    add jsonutil's method getJsonValueRec

--- src/main/java/com/httputil/apitest/util/JsonUtil.java
+++ src/main/java/com/httputil/apitest/util/JsonUtil.java
@@ -1,5 +1,6 @@
 package com.httputil.apitest.util;
 
+import com.alibaba.fastjson.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -59,5 +60,47 @@ public class JsonUtil {
             var5.printStackTrace();
             return null;
         }
+    }
+
+    /**
+     * 获取json串某一字段的值
+     *
+     * @param response 接口返回的json值转换后
+     * @param keys     调用示例 String str = JsonUtils.getJsonValueRec(string,"data.departments.subDepartments.name",0);
+     * @param index    需要取列表中第几个的值，一般传0就可以
+     * @return 返回String类型的结果值
+     */
+    public static String getJsonValueRec(String response, String keys, int index) {
+        String[] key = keys.split("\\.");
+        if (key.length == 1) {
+            if (response.startsWith("[")) {
+                return parseJsonArray(response, index, key[0]);
+            }
+            if (response.startsWith("{")) {
+                return parseJsonObject(response, key[0]);
+            }
+        } else if (key.length > 1) {
+            if (response.startsWith("[")) {
+                response = parseJsonArray(response, index, key[0]);
+                keys = keys.substring(key[0].length() + 1);
+                return getJsonValueRec(response, keys, index);
+            }
+            if (response.startsWith("{")) {
+                response = parseJsonObject(response, key[0]);
+                keys = keys.substring(key[0].length() + 1);
+                return getJsonValueRec(response, keys, index);
+            }
+        }
+        return "No corresponding value found";
+    }
+
+    private static String parseJsonArray(String response, int index, String key) {
+        JSONArray resultArray = com.alibaba.fastjson.JSONObject.parseArray(response);
+        return resultArray.getJSONObject(index).getString(key);
+    }
+
+    private static String parseJsonObject(String response, String key) {
+        com.alibaba.fastjson.JSONObject jsonObject = com.alibaba.fastjson.JSONObject.parseObject(response);
+        return jsonObject.get(key).toString();
     }
 }
